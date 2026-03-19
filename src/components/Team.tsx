1d02fa205dbe02eab780fd0d00d3874c98e8ebea
abhisheksonias <abhisheksonisince2003@gmail.com>
2026-03-19 10:49:00 +0530

    teams

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -3,17 +3,16 @@
 import HeroAnimations from "@/lib/heroAnimation";
 import HeroBackground from "@/components/HeroBackground";
 import TeamMember, { teamMembers } from "./TeamMember";
-import Link from "next/link"
 
 const Team = () => {
   return (
     <div className="flex-1 bg-white  xl:mx-10">
       <HeroAnimations />
-      {/* Team Header Section */}
-      <section className="relative py-4 px-4 sm:px-8 md:px-12 lg:px-16 overflow-hidden">
+      {/* Team Hero Section */}
+      <section className="relative px-4 pt-4 pb-10 sm:px-8 md:px-12 lg:px-16 overflow-hidden">
         <HeroBackground />
-        <div className="max-w-6xl mx-auto">
-          <div className="text-center mb-8">
+        <div className=" max-w-6xl mx-auto">
+          <div className="text-center">
             <h1
               className="hero-text-title hero-title mb-[6px] text-center text-[#163C0F] text-[34px] leading-[38px] tracking-[1px] sm:text-[40px] sm:leading-[44px] md:mt-[104px] lg:text-[46px] lg:leading-[50px] lg:tracking-[2px]"
               style={{
@@ -24,8 +23,12 @@ const Team = () => {
               Leading Tax Experts
             </h1>
           </div>
+        </div>
+      </section>
 
-          {/* Team Grid */}
+      {/* Team Cards Section */}
+      <section className="px-4 pb-8 mt-8 sm:px-8 md:px-12 lg:px-16">
+        <div className="max-w-6xl mx-auto">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             {Object.values(teamMembers).map((member) => (
               <TeamMember key={member.id} member={member} />
